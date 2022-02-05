b2ca7de548de2b7e733251c0b2951e4919986bb0
Rachit-veronica <rachitgupta7007@gmail.com>
2022-02-05 21:52:11 +0530

    main file api set

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
 app.get("/",(req, res) => {
-    res.send("Hello World");
-});
-
-app.get("/job",(req, res)=>{
     res.send(importData);
 });
 
